da03a624065c3028f79f2b3f58bc72d583f60dab
ricardo <ricardofelgueiras1993@gmail.com>
2024-08-14 16:52:28 +0200

    parsing RSS feed into objects

--- lib/ui/view_model/zapping_provider.dart
+++ lib/ui/view_model/zapping_provider.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/widgets.dart';
 import 'package:zapping_flutter/data/repository/implementation/zapping_repository_impl.dart';
+import 'package:zapping_flutter/domain/match/match_parser_impl.dart';
+import 'package:zapping_flutter/domain/model/my_match.dart';
 import 'package:zapping_flutter/main.dart';
 
 class ZappingProvider extends ChangeNotifier {
   // todo this should be injected
   late final _zappingRepository = ZappingRepositoryImpl();
+  late final _matchParser = MatchParserImpl();
+
+  Iterable<MyMatch> _matches = const [];
+
+  List<MyMatch> get matches {
+    return List<MyMatch>.unmodifiable(_matches);
+  }
 
   void getMatches() async {
     final articleList = await _zappingRepository.getMatches(zappingUrl);
 
-    // todo parse the matches
+    final matchesWithNulls = articleList.map(
+      (article) {
+        return _matchParser.parse(article);
+      },
+    );
+
+    _matches = matchesWithNulls.nonNulls;
+
+    notifyListeners();
   }
 }
