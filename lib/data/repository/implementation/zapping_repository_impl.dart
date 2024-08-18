5a6757883c0f9c745130821773812ee73415f1cd
Ricardo Pinto <ricardofelgueiras1993@gmail.com>
2024-08-18 22:42:30 +0200

    changing charset from the zapping response (#7)

--- lib/data/repository/implementation/zapping_repository_impl.dart
+++ lib/data/repository/implementation/zapping_repository_impl.dart
@@ -1,21 +1,18 @@
 import 'package:dart_rss/dart_rss.dart';
-import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
 import 'package:zapping_flutter/data/repository/contract/zapping_repository.dart';
 import 'package:zapping_flutter/data/repository/model/my_article.dart';
 
 final class ZappingRepositoryImpl implements ZappingRepository {
-  late final _dio = Dio();
-
   // todo handle exceptions
   @override
   Future<List<MyArticle>> getMatches(String url) async {
-    // the user-agent part is because ZeroZero was giving us error 429 with the default user-agent
-    final response = await _dio.get(url,
-        options: Options(
-          headers: {"user-agent": ""},
-        ));
+    // I am using http because dio was not correctly decoding accents
+    // the user-agent part is because the website was giving us error 429 with the default user-agent
+    final response =
+        await http.get(Uri.parse(url), headers: {"user-agent": ""});
 
-    final rssFeed = RssFeed.parse(response.data.toString());
+    final rssFeed = RssFeed.parse(response.body);
 
     final rssItems = rssFeed.items.where((item) {
       return item.pubDate != null && item.title != null;
