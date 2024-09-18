d19a39349884d5ce8bc1dea72fc0a5847edc8746
Ricardo Pinto <ricardofelgueiras1993@gmail.com>
2024-09-19 00:00:44 +0200

    created one test for the match_parser_impl.dart (#14)

---
+++ test/domain/match/match_parser_impl_test.dart
@@ -0,0 +1,34 @@
+import 'package:test/test.dart';
+import 'package:zapping_flutter/data/repository/model/my_article.dart';
+import 'package:zapping_flutter/domain/match/match_parser_impl.dart';
+
+void main() {
+  late MatchParserImpl matchParser;
+
+  //    "Peñarol x Atlético Mineiro - 14/05 23:00 - SportTv1"
+  const validArticleDateString = "Tue, 14 May 2024 23:00:00";
+  const validArticleHomeTeam = "Peñarol";
+  const validArticleAwayTeam = "Atlético Mineiro";
+  const validArticleChannel = "SportTv1";
+  const validArticleTitle =
+      "$validArticleHomeTeam x $validArticleAwayTeam - 14/05 23:00 - $validArticleChannel";
+  const validArticle = MyArticle(validArticleDateString, validArticleTitle);
+
+  setUp(() {
+    matchParser = MatchParserImpl();
+  });
+
+  test("parse valid article", () {
+    final match = matchParser.parse(validArticle);
+
+    expect(match, isNotNull);
+    if (match != null) {
+      expect(match.originalText, validArticleTitle);
+      expect(match.homeTeam, validArticleHomeTeam);
+      expect(match.awayTeam, validArticleAwayTeam);
+      expect(match.date, DateTime(2024, 5, 14, 23, 0, 0));
+      expect(match.channel, validArticleChannel);
+      expect(match.originalText, validArticleTitle);
+    }
+  });
+}
