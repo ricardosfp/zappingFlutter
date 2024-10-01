68354abe5ac2249751eb5b8b02d1ddbf0ec839ed
Ricardo Pinto <ricardofelgueiras1993@gmail.com>
2024-10-01 14:46:29 +0100

    first tests for the ZappingRepositoryImpl (#21)

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:pretty_http_logger/pretty_http_logger.dart';
 import 'package:zapping_flutter/di/di.config.dart';
 
 final getIt = GetIt.instance;
@@ -9,3 +10,10 @@ final getIt = GetIt.instance;
   asExtension: true, // default
 )
 void configureDependencies() => getIt.init();
+
+@module
+abstract class Module {
+  @lazySingleton
+  HttpWithMiddleware getHttp() => HttpWithMiddleware.build(
+      middlewares: [HttpLogger(logLevel: LogLevel.BODY)]);
+}
---
+++ test/data/repository/implementation/zapping_repository_impl_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:pretty_http_logger/pretty_http_logger.dart';
+import 'package:zapping_flutter/data/repository/contract/get_articles_result.dart';
+import 'package:zapping_flutter/data/repository/implementation/zapping_repository_impl.dart';
+
+import 'zapping_repository_impl_test.mocks.dart';
+
+@GenerateMocks([HttpWithMiddleware])
+void main() {
+  late ZappingRepositoryImpl zappingRepository;
+
+  // setUp(() {
+  //   zappingRepository = ZappingRepositoryImpl();
+  // });
+
+  test("ClientException in the http client returns GetArticlesHttpError",
+      () async {
+    final mockHttp = MockHttpWithMiddleware();
+
+    when(mockHttp.get(any, headers: anyNamed("headers")))
+        .thenThrow(ClientException(""));
+
+    zappingRepository = ZappingRepositoryImpl(http: mockHttp);
+
+    expect(
+        await zappingRepository.getArticles(""), isA<GetArticlesHttpError>());
+  });
+
+  // todo I should mock both the http client and the parser
+  // test(
+  //     "good response from the http client and good parsing returns GetArticlesSuccess",
+  //     () {
+  //   final mockHttp = MockHttpWithMiddleware();
+  //
+  //   when(mockHttp.get(any, headers: anyNamed("headers")))
+  //       .thenAnswer((_) async => Response("", 200));
+  //
+  //   zappingRepository = ZappingRepositoryImpl(http: mockHttp);
+  // });
+}
