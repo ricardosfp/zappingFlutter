1dbbdb9acfdcc3ef4c67f3162f58c6ca0931e0ff
Ricardo Pinto <ricardofelgueiras1993@gmail.com>
2026-03-05 21:01:45 +0100

    changed the UI of the matches (#42)

--- lib/ui/zapping/widget/zapping_item.dart
+++ lib/ui/zapping/widget/zapping_item.dart
@@ -5,21 +5,27 @@ import 'package:zapping_flutter/domain/match/model/my_match.dart';
 class ZappingItem extends StatelessWidget {
   final MyMatch _match;
 
-  static final _dateFormat = DateFormat("HH:mm");
-
-  static const _textStyle = TextStyle(color: Color(0xff000000), fontSize: 16);
-
   const ZappingItem({super.key, required MyMatch match}) : _match = match;
+
+  static final _dateFormat = DateFormat("HH:mm");
+  static const _textStyle = TextStyle(color: Color(0xff000000), fontSize: 16);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text("${_match.homeTeam} x ${_match.awayTeam}", style: _textStyle),
-        Text(_dateFormat.format(_match.date), style: _textStyle),
-        Text(_match.channel, style: _textStyle),
-      ],
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Theme.of(context).colorScheme.inversePrimary),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("${_match.homeTeam} x ${_match.awayTeam}", style: _textStyle),
+          Text(_dateFormat.format(_match.date), style: _textStyle),
+          Text(_match.channel, style: _textStyle),
+        ],
+      ),
     );
   }
 }
